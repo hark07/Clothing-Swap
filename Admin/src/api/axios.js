aff66cbe461341afc5cdcc7e2472cbcdc8d4ff8c
Hark Dhami <harkdhami08@gmail.com>
2026-07-17 19:18:14 +0545

    Clothes Swap

--- Admin/src/api/axios.js
+++ Admin/src/api/axios.js
@@ -1,35 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  withCredentials: true,
+  baseURL: "https://clothing-swap.onrender.com/api",
 });
 
-API.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    return config;
-  },
-  (error) => Promise.reject(error),
-);
-
-API.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem("token");
-    }
-
-    return Promise.reject(error);
-  },
-);
+  return config;
+});
 
 export default API;
